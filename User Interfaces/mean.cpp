7fa8911b4df85a43447dc74a0366d3d670cfcf01
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2024-10-09 15:28:21 +0000

    User Interface: Introduction to C++ completed

--- User Interfaces/mean.cpp
+++ User Interfaces/mean.cpp
@@ -14,6 +14,20 @@ void read_data(istream& input, vector<float>& data)
 
 }
 
+float mean_value(vector<float>& data)
+{
+    int i;
+    float sum = 0;
+    float mean;
+
+    for (i=0; i < data.size(); i++)
+    {
+    sum += data[i];
+    }
+    mean = sum/data.size();
+    return mean;
+}
+
 int main(int argc, char* argv[])
 {
     if (argc != 2)
@@ -32,6 +46,9 @@ int main(int argc, char* argv[])
     vector<float> data;
 
     read_data(infile, data);
-    cout << data.pop_back() << endl;
+
+    cout << data.size() << " values read from the file." << endl;
+    cout << "mean value = " << mean_value(data) << endl;
+
     return 0;
 }
