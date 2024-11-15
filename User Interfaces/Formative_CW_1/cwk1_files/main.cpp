c8a3f58ef534677e7b82c623d63eb7a854a8b1f5
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2024-11-15 08:07:35 +0000

    UI Formative CW 1

--- User Interfaces/Formative_CW_1/cwk1_files/main.cpp
+++ User Interfaces/Formative_CW_1/cwk1_files/main.cpp
@@ -1,6 +1,23 @@
 // COMP2811 Coursework 1: quaketool application
 
+#include <iostream>
+#include "dataset.hpp"
+#include "argparse.hpp"
+
+using namespace std;
+
 int main(int argc, char* argv[])
 {
+  if (argc != 2)
+  {
+    fprintf(2, "Usage: ./main <file_name>");
+  }
+
+  argparse::ArgumentParser program("main");
+
+  program.add_argument("filename")
+    .help("name of CSV file containing quake data")
+    .metavar("CSV_FILE");
+
   return 0;
 }
