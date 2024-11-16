c42f09554f7536eed688dfff7a1ff3e014909217
Sc23aj2 <146622139+Sc23aj2@users.noreply.github.com>
2024-11-16 23:56:37 +0000

    CW1 UI

--- User Interfaces/Formative_CW_1/cwk1_files/dataset.cpp
+++ User Interfaces/Formative_CW_1/cwk1_files/dataset.cpp
@@ -7,14 +7,16 @@ using namespace std;
 
 void QuakeDataset::loadData(const string& file)
 {
-    csv::CSVReader reader("data/test0.csv");
+    csv::CSVReader reader(file);
+    
+    data.clear();
     for (auto& row: reader) 
     {
-        string time = row["A"].get<>();
-        double latitude = row["B"].get<double>();
-        double longitude = row["C"].get<double>();
-        double depth = row["C"].get<double>();
-        double magnitude = row["C"].get<double>();
+        string time = row["time"].get<>();
+        double latitude = row["latitude"].get<double>();
+        double longitude = row["longitude"].get<double>();
+        double depth = row["depth"].get<double>();
+        double magnitude = row["mag"].get<double>();
 
         data.push_back(Quake(time, latitude, longitude, depth, magnitude));
     }
@@ -43,16 +45,60 @@ Quake QuakeDataset::strongest() const
 
 Quake QuakeDataset::shallowest() const
 {
-    return Quake("aaa", 0, 0, 0, 0);
+    int i;
+    if (data.size() == 0)
+    {
+        throw out_of_range("Cannot find shallowest for empty dataset.");
+    }
+    
+    Quake shallowest = data[0];
+
+    for (i = 0; i < data.size(); i ++)
+    {
+        if(data[i].getDepth() < shallowest.getDepth())
+        {
+            shallowest = data[i];
+        }
+    }
+
+    return shallowest;
 }
 
 double QuakeDataset::meanDepth() const
 {
-    return 0;
+    int i;
+    if (data.size() == 0)
+    {
+        throw out_of_range("Cannot find mean depth for empty dataset.");
+    }
+    
+    double sum = 0;
+
+    for (i = 0; i < data.size(); i ++)
+    {
+        sum += data[i].getDepth();
+    }
+
+    double mean_depth = sum / data.size();
+    return mean_depth;
 }
 
 double QuakeDataset::meanMagnitude() const
 {
-    return 0;
+    int i;
+    if (data.size() == 0)
+    {
+        throw out_of_range("Cannot find mean magnitude for empty dataset.");
+    }
+    
+    double sum = 0;
+
+    for (i = 0; i < data.size(); i ++)
+    {
+        sum += data[i].getMagnitude();
+    }
+
+    double mean_magnitude = sum / data.size();
+    return mean_magnitude;
 }
 
--- User Interfaces/Formative_CW_1/cwk1_files/main.cpp
+++ User Interfaces/Formative_CW_1/cwk1_files/main.cpp
@@ -1,6 +1,7 @@
 // COMP2811 Coursework 1: quaketool application
 
 #include <iostream>
+#include <fstream>
 #include "dataset.hpp"
 #include "argparse.hpp"
 
@@ -13,6 +14,24 @@ int main(int argc, char* argv[])
   program.add_argument("filename")
     .help("name of CSV file containing quake data")
     .metavar("CSV_FILE");
-  program.parse_args(argc, argv);
+
+  try 
+  {
+    program.parse_args(argc, argv);
+  }
+  catch (const std::exception& err)
+  {
+    cerr << err.what() << endl;
+    cerr << program;
+    return 1;
+  }
+
+  auto file = program.get("filename");
+  
+  QuakeDataset quakedataset;
+
+  quakedataset.loadData(file);
+
+  
   return 0;
 }
